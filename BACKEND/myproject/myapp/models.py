cb5a494db3bfb626dcfb504900622b19e00e5ce9
mamata <raimamata.com>
2025-12-18 03:43:34 +0545

    sec

--- BACKEND/myproject/myapp/models.py
+++ BACKEND/myproject/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+class Book(models.Model):
+    title = models.CharField(max_length=200)        # Book title
+    author = models.CharField(max_length=100)       # Author name
+    published_date = models.DateField()             # Publication date
+    price = models.DecimalField(max_digits=6, decimal_places=2)  # Price of the book
+    description = models.TextField(blank=True)     # Optional description
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.
