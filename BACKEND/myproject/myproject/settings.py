79b1ef700bf7aee1a2e00def6657ad2eb14af6be
mamata <raimamata.com>
2025-12-29 07:10:00 +0545

    Clean repo: remove pycache, venv and add gitignore

--- BACKEND/myproject/myproject/settings.py
+++ BACKEND/myproject/myproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'rest_framework',
+    'drf_yasg',
 
 ]
 
@@ -118,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
--- BACKEND/myproject/myproject/urls.py
+++ BACKEND/myproject/myproject/urls.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
-from django.urls import path, include   # 👈 path is here
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from myapp import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Pastebin API",
+        default_version='v1',
+        description="API documentation",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
     path('', views.home),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
 
 if settings.DEBUG:
